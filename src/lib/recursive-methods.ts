fbcb58641a555561db016ac9257ad6aa9adae3d7
Ricardo <Correia.ferreira.ricardo@gmail.com>
2022-11-04 14:26:30 +0100

    work doc

--- src/lib/recursive-methods.ts
+++ src/lib/recursive-methods.ts
@@ -2,12 +2,13 @@
 /* eslint-disable functional/no-let */
 /* eslint-disable functional/no-loop-statement */
 /*
+ * cherche dans un arbre les noeuds qui correspondent au predicat et les renvoie avec leur parents en leurs enfants
  */
-export function filterList<T>(
+export function filterTree<T>(
   // eslint-disable-next-line functional/prefer-readonly-type
   listeComplete: T[],
   predicat: (el: T) => boolean,
-  getMyId: (el: T) => number,
+  getId: (el: T) => number,
   getParentId: (el: T) => number
   // eslint-disable-next-line functional/prefer-readonly-type
 ): T[] {
@@ -16,19 +17,19 @@ export function filterList<T>(
 
   //On supprime les résultats de la liste
   listeComplete = listeComplete.filter(
-    (o) => !res.some((r) => getMyId(r) == getMyId(o))
+    (o) => !res.some((r) => getId(r) === getId(o))
   );
 
   let parents = resInitial;
   while (parents.length > 0) {
     const nouveauxParents = listeComplete.filter((o) =>
-      parents.some((r) => getParentId(r) == getMyId(o))
+      parents.some((r) => getParentId(r) === getId(o))
     );
     //B+A
     if (nouveauxParents.length > 0) {
       res.push(...nouveauxParents);
       listeComplete = listeComplete.filter(
-        (o) => !nouveauxParents.some((r) => getMyId(r) == getMyId(o))
+        (o) => !nouveauxParents.some((r) => getId(r) === getId(o))
       );
     }
     parents = nouveauxParents;
@@ -37,13 +38,13 @@ export function filterList<T>(
   let enfants = resInitial;
   while (enfants.length > 0) {
     const nouveauxEnfants = listeComplete.filter((o) =>
-      enfants.some((r) => getMyId(r) == getParentId(o))
+      enfants.some((r) => getId(r) === getParentId(o))
     );
     //B+A
     if (nouveauxEnfants.length > 0) {
       res.push(...nouveauxEnfants);
       listeComplete = listeComplete.filter(
-        (o) => !nouveauxEnfants.some((r) => getMyId(r) == getMyId(o))
+        (o) => !nouveauxEnfants.some((r) => getId(r) === getId(o))
       );
     }
     enfants = nouveauxEnfants;
