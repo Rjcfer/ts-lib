fb8c94d8bb381d88244f9c9e17a4ac82f93435fb
Ricardo <Correia.ferreira.ricardo@gmail.com>
2022-11-04 14:38:38 +0100

    work

--- src/lib/recursive-methods.ts
+++ src/lib/recursive-methods.ts
@@ -2,53 +2,72 @@
 /* eslint-disable functional/no-let */
 /* eslint-disable functional/no-loop-statement */
 /*
- * cherche dans un arbre les noeuds qui correspondent au predicat et les renvoie avec leur parents en leurs enfants
+ * cherche dans un arbre les noeuds qui correspondent au predicat et les renvoie avec leurs parents en leurs enfants
  */
+//A
+// - B
+// - C
+// - - D
+//E
+// - F
+// - G
+// - - H
+// - - J
+
 export function filterTree<T>(
-  // eslint-disable-next-line functional/prefer-readonly-type
-  listeComplete: T[],
-  predicat: (el: T) => boolean,
-  getId: (el: T) => number,
-  getParentId: (el: T) => number
+  options: {
+    // eslint-disable-next-line functional/prefer-readonly-type
+    readonly nodes: T[];
+    readonly predicate: (el: T) => boolean;
+    readonly getId: (el: T) => number;
+    readonly getParentId: (el: T) => number;
+    readonly withParents: boolean;
+    readonly withChildren: boolean;
+  }
   // eslint-disable-next-line functional/prefer-readonly-type
 ): T[] {
-  const res = listeComplete.filter(predicat);
+  options = Object.assign({ withParents: true, withChildren: true }, options);
+  const res = options.nodes.filter(options.predicate);
   const resInitial = [...res];
 
   //On supprime les résultats de la liste
-  listeComplete = listeComplete.filter(
-    (o) => !res.some((r) => getId(r) === getId(o))
+  let nodesFiltered = options.nodes.filter(
+    (o) => !res.some((r) => options.getId(r) === options.getId(o))
   );
 
-  let parents = resInitial;
-  while (parents.length > 0) {
-    const nouveauxParents = listeComplete.filter((o) =>
-      parents.some((r) => getParentId(r) === getId(o))
-    );
-    //B+A
-    if (nouveauxParents.length > 0) {
-      res.push(...nouveauxParents);
-      listeComplete = listeComplete.filter(
-        (o) => !nouveauxParents.some((r) => getId(r) === getId(o))
+  if (options.withParents) {
+    let parents = resInitial;
+    while (parents.length > 0) {
+      const nouveauxParents = nodesFiltered.filter((o) =>
+        parents.some((r) => options.getParentId(r) === options.getId(o))
       );
+      //B+A
+      if (nouveauxParents.length > 0) {
+        res.push(...nouveauxParents);
+        nodesFiltered = nodesFiltered.filter(
+          (o) =>
+            !nouveauxParents.some((r) => options.getId(r) === options.getId(o))
+        );
+      }
+      parents = nouveauxParents;
     }
-    parents = nouveauxParents;
   }
-
-  let enfants = resInitial;
-  while (enfants.length > 0) {
-    const nouveauxEnfants = listeComplete.filter((o) =>
-      enfants.some((r) => getId(r) === getParentId(o))
-    );
-    //B+A
-    if (nouveauxEnfants.length > 0) {
-      res.push(...nouveauxEnfants);
-      listeComplete = listeComplete.filter(
-        (o) => !nouveauxEnfants.some((r) => getId(r) === getId(o))
+  if (options.withChildren) {
+    let enfants = resInitial;
+    while (enfants.length > 0) {
+      const nouveauxEnfants = nodesFiltered.filter((o) =>
+        enfants.some((r) => options.getId(r) === options.getParentId(o))
       );
+      //B+A
+      if (nouveauxEnfants.length > 0) {
+        res.push(...nouveauxEnfants);
+        nodesFiltered = nodesFiltered.filter(
+          (o) =>
+            !nouveauxEnfants.some((r) => options.getId(r) === options.getId(o))
+        );
+      }
+      enfants = nouveauxEnfants;
     }
-    enfants = nouveauxEnfants;
   }
-
   return res; //ABCDG
 }
